f48368563d4f43e73ca10ad0304877262b3e4441
lzxm160 <lzxm160@sina.com>
2019-11-29 16:21:38 +0800

    x

---
+++ first-network/scripts/scriptonlycallforlaoyu.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Build your first network (BYFN) end-to-end test"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+CC_SRC_LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+NO_CHAINCODE="$6"
+: ${CHANNEL_NAME:="foodchannel"}
+: ${DELAY:="3"}
+: ${CC_SRC_LANGUAGE:="go"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+: ${NO_CHAINCODE:="false"}
+CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=20
+PACKAGE_ID=""
+
+if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ]; then
+	CC_RUNTIME_LANGUAGE=golang
+	CC_SRC_PATH="github.com/hyperledger/fabric-samples/chaincode/abstore/go/"
+elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
+	CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/javascript/"
+elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
+	CC_RUNTIME_LANGUAGE=java
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/java/"
+else
+	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
+	echo Supported chaincode languages are: go, javascript, java
+	exit 1
+fi
+
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+
+	# Invoke chaincode on peer0.org1
+	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+	chaincodeInvoke 0 0 1 #"invoke","a","b","10"
+
+	# Query chaincode on peer0.org1
+	echo "Querying chaincode on peer0.org1..."
+	chaincodeQuery 0 1 80
+
+exit 0
